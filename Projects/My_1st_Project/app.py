49b5022edf0a754e1097fa448835ac49f4e6a214
Matt Bae <65493642+MattBae-Coding@users.noreply.github.com>
2020-07-09 00:36:50 +0900

    0708

--- Projects/My_1st_Project/app.py
+++ Projects/My_1st_Project/app.py
@@ -1,18 +1,18 @@
 from flask import Flask, render_template, request, jsonify
 app = Flask(__name__) #flaks를 사용하겠다는 말
-
+#HTML
 @app.route('/') #5000 뒤에 붙는것
 def home():
    return render_template('index.html')
-@app.route('/') #5000 뒤에 붙는것
+@app.route('/enroll') #5000 뒤에 붙는것
 def enroll():
    return render_template('enroll.html')
 
-@app.route('/test', methods=['POST'])
-def test_post():
-   title_receive = request.form['title_give']
-   print(title_receive)
-   return jsonify({'result':'success', 'msg': '이 요청은 POST!'})
+
+#API
+@app.route('/save', methods=['POST'])
+def save():
+   return jsonify({'result':'success', 'msg': '저장완료'})
 
 @app.route('/test', methods=['GET'])
 def test_get():
@@ -20,5 +20,7 @@ def test_get():
    print(title_receive)
    return jsonify({'result':'success', 'msg': '이 요청은 GET!'})
 
+
+#flaks를 돌려주는 친구 - 계속 돌아감 
 if __name__ == '__main__':  
-   app.run('0.0.0.0',port=5000,debug=True) #flaks를 돌려주는 친구 - 계속 돌아감 
+   app.run('0.0.0.0',port=5000,debug=True) 
